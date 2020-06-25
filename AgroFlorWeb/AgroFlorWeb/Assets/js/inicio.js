8f77f0324e2885141e8ae32655857470d309fe5a
Santiago outlook.es <santiagogilrea1998@outlook.es>
2020-06-25 10:30:57 -0500

    Menu desplegable

---
+++ AgroFlorWeb/AgroFlorWeb/Assets/js/inicio.js
@@ -0,0 +1,16 @@
+﻿var btn_user = document.getElementById('userbtn'),
+    options_user = document.getElementById('useroptions'),
+    menu_options = document.getElementById('usermenu'),
+    control = 1;
+
+btn_user.addEventListener('click', function () {
+    if (control == 1) {
+        options_user.classList.add('active');
+        menu_options.classList.add('active');
+        control++;
+    } else {
+        options_user.classList.remove('active');
+        menu_options.classList.remove('active');
+        control = 1;
+    }
+}); 
